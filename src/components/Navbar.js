6586bc27c2dfdb26032ae714da6467dac819ba95
lcroce28 <lcj10@alumnes.udl.cat>
2026-04-28 22:37:00 +0200

    navbar afegida

---
+++ src/components/Navbar.js
@@ -0,0 +1,35 @@
+import { Link } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <nav style={styles.nav}>
+      <h2 style={styles.logo}>🔥 Drac Olivé 🐉</h2>
+
+      <div style={styles.links}>
+        <Link to="/">Inici</Link>
+        <Link to="/colla">Colla</Link>
+        <Link to="/events">Events</Link>
+      </div>
+    </nav>
+  );
+}
+
+const styles = {
+  nav: {
+    display: "flex",
+    justifyContent: "space-between",
+    padding: "15px",
+    background: "black",
+    color: "white",
+    alignItems: "center",
+  },
+  logo: {
+    margin: 0,
+  },
+  links: {
+    display: "flex",
+    gap: "15px",
+  },
+};
+
+export default Navbar;
