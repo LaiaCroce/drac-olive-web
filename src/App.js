0d8df548dcba6c95cdbdbc02cb2eea7b09701085
lcroce28 <lcj10@alumnes.udl.cat>
2026-04-28 22:37:36 +0200

    fitxers

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
+import Colla from "./pages/Colla";
+import Events from "./pages/Events";
 
 function App() {
   return (
-    <div>
-      <Home />
-    </div>
+    <BrowserRouter>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/colla" element={<Colla />} />
+        <Route path="/events" element={<Events />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
