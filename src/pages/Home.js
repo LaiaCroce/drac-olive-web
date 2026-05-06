d0db283b61a3513f89ede97836f774f8e560a133
lcroce28 <lcj10@alumnes.udl.cat>
2026-05-06 15:56:54 +0200

    add story at home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,14 @@
 import Hero from "../components/Hero/Hero";
 import NextEvent from "../components/NextEvent/NextEvent";
 import HomeSections from "../components/HomeSections/HomeSections";
+import StorySection from "../components/StorySection/StorySection";
 
 export default function Home() {
   return (
     <>
       <Hero />
       <NextEvent />
+      <StorySection />
       <HomeSections />
     </>
   );
