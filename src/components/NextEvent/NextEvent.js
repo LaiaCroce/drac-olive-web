c25697beed9522de2e77c51b994e6dcd2dbd30b8
lcroce28 <lcj10@alumnes.udl.cat>
2026-05-06 17:57:25 +0200

    mostrar proxim esdeveniment al home

--- src/components/NextEvent/NextEvent.js
+++ src/components/NextEvent/NextEvent.js
@@ -1,11 +1,61 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { db } from "../../services/firebase";
+import { collection, getDocs, query, orderBy, where, Timestamp, limit } from "firebase/firestore";
 import "./NextEvent.css";
 
 function NextEvent() {
+  const [event, setEvent] = useState(null);
+
+  useEffect(() => {
+    const fetchNextEvent = async () => {
+      try {
+        const today = Timestamp.now();
+
+        const eventsQuery = query(
+          collection(db, "events"),
+          where("data", ">=", today),
+          orderBy("data", "asc"),
+          limit(1)
+        );
+
+        const querySnapshot = await getDocs(eventsQuery);
+
+        if (!querySnapshot.empty) {
+          const doc = querySnapshot.docs[0];
+          setEvent({
+            id: doc.id,
+            ...doc.data(),
+          });
+        }
+      } catch (error) {
+        console.error("Error carregant proper event:", error);
+      }
+    };
+
+    fetchNextEvent();
+  }, []);
+
+  if (!event) {
+    return (
+      <section className="next-event">
+        <div className="next-event-intro">
+          <p>Propera sortida 🔥</p>
+          <h2>No hi ha sortides programades</h2>
+          <Link to="/events" className="btn btn-secondary">
+            Veure agenda
+          </Link>
+        </div>
+      </section>
+    );
+  }
+
+  const eventDate = event.data.toDate();
+
   return (
     <section className="next-event">
       <div className="next-event-intro">
-        <p>Propera sortida </p>
+        <p>Propera sortida 🔥</p>
         <h2>No et perdis el proper correfoc!</h2>
         <Link to="/events" className="btn btn-secondary">
           Veure agenda
@@ -14,16 +64,22 @@ function NextEvent() {
 
       <div className="next-event-card">
         <div className="next-event-date">
-          <span>29</span>
-          <small>Octubre</small>
+          <span>{eventDate.getDate()}</span>
+          <small>
+            {eventDate.toLocaleDateString("ca-ES", { month: "short" })}
+          </small>
         </div>
 
         <div className="next-event-info">
-          <h3>Correfoc Nit de les Bèsties 3 </h3>
-          <p>📍 Plaça Catalunya, Olesa de Montserrat</p>
+          <h3>{event.titol}</h3>
+          <p>📍 {event.lloc}</p>
         </div>
 
-        <div className="next-event-time">20:30h</div>
+        <div className="next-event-time">
+          {eventDate.toLocaleTimeString("ca-ES", { 
+            hour: "2-digit", 
+            minute: "2-digit" })}
+        </div>
       </div>
     </section>
   );
