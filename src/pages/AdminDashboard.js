e75ceb0591fec2aa3d07198122aa135ab2f4fd6f
lcroce28 <lcj10@alumnes.udl.cat>
2026-05-28 12:23:10 +0200

    afegit logout admin

--- src/pages/AdminDashboard.js
+++ src/pages/AdminDashboard.js
@@ -1,13 +1,30 @@
 import "./AdminDashboard.css";
 import CreateEventForm from "../components/Admin/CreateEventForm";
+import { signOut } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
+import { auth } from "../services/firebase";
 
 export default function AdminDashboard() {
+  const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    try {
+      await signOut(auth);
+      navigate("/admin");
+    } catch (error) {
+      console.error("Error signing out:", error);
+    }
+  };
+
   return (
     <main className="admin-dashboard">
       <section className="admin-dashboard-header">
         <p>Zona admin</p>
         <h1>Panell de control</h1>
         <span>Gestiona els continguts del Drac Olivé.</span>
+        <button className="admin-logout" onClick={handleLogout}>
+          Tancar sessió
+        </button>
       </section>
 
       <section className="admin-dashboard-grid">
