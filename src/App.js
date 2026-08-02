76748eefd68955572f58ab93c35896e4b43a32ad
lcroce28 <lcj10@alumnes.udl.cat>
2026-08-02 13:57:22 +0200

    transicions entre pagines

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
+import { useEffect } from "react";
 import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
-
 import { AuthProvider } from "./context/AuthContext";
+import "./App.css";
 
 import Navbar from "./components/Navbar/Navbar";
 import ProtectedRoute from "./components/ProtectedRoute";
@@ -32,76 +33,84 @@ function Layout() {
   const location = useLocation();
   const isAdminPage = location.pathname.startsWith("/admin");
 
+  useEffect(() => {
+  window.scrollTo({
+    top: 0,
+    behavior: "auto",
+  });
+}, [location.pathname]);
+
   return (
     <>
       {!isAdminPage && <Navbar />}
-
-      <Routes>
-        {/* WEB PÚBLICA */}
-        <Route path="/" element={<Home />} />
-        <Route path="/qui-som" element={<QuiSom />} />
-        <Route path="/historia" element={<Historia />} />
-        <Route path="/colla" element={<Colla />} />
-        <Route path="/colla/piro" element={<Piro />} />
-        <Route path="/colla/fogosons" element={<Fogosons />} />
-        <Route path="/events" element={<Events />} />
-        <Route path="/galeria" element={<Gallery />} />
-        <Route path="/merch" element={<Merch />} />
-        <Route path="/contacte" element={<Contacte />} />
-        <Route path="/noticies" element={<Noticies />} />
-        {/* Conservem la pàgina del Drac com a ruta interna */}
-        <Route path="/drac" element={<Drac />} />
-        {/* ADMIN */}
-        <Route path="/admin" element={<AdminLogin />} />
-        <Route
-          path="/admin/dashboard"
-          element={
-            <ProtectedRoute>
-              <AdminDashboard />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/admin/agenda"
-          element={
-            <ProtectedRoute>
-              <AdminAgenda />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/admin/galeria"
-          element={
-            <ProtectedRoute>
-              <AdminGallery />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/admin/noticies"
-          element={
-            <ProtectedRoute>
-              <AdminNews />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/admin/collaboradors"
-          element={
-            <ProtectedRoute>
-              <AdminCollaborators />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/admin/merch"
-          element={
-            <ProtectedRoute>
-              <AdminMerch />
-            </ProtectedRoute>
-          }
-        />
-      </Routes>
+      <div key={location.pathname} className="page-transition">
+        <Routes>
+          {/* WEB PÚBLICA */}
+          <Route path="/" element={<Home />} />
+          <Route path="/qui-som" element={<QuiSom />} />
+          <Route path="/historia" element={<Historia />} />
+          <Route path="/colla" element={<Colla />} />
+          <Route path="/colla/piro" element={<Piro />} />
+          <Route path="/colla/fogosons" element={<Fogosons />} />
+          <Route path="/events" element={<Events />} />
+          <Route path="/galeria" element={<Gallery />} />
+          <Route path="/merch" element={<Merch />} />
+          <Route path="/contacte" element={<Contacte />} />
+          <Route path="/noticies" element={<Noticies />} />
+          {/* Conservem la pàgina del Drac com a ruta interna */}
+          <Route path="/drac" element={<Drac />} />
+          {/* ADMIN */}
+          <Route path="/admin" element={<AdminLogin />} />
+          <Route
+            path="/admin/dashboard"
+            element={
+              <ProtectedRoute>
+                <AdminDashboard />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/admin/agenda"
+            element={
+              <ProtectedRoute>
+                <AdminAgenda />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/admin/galeria"
+            element={
+              <ProtectedRoute>
+                <AdminGallery />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/admin/noticies"
+            element={
+              <ProtectedRoute>
+                <AdminNews />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/admin/collaboradors"
+            element={
+              <ProtectedRoute>
+                <AdminCollaborators />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/admin/merch"
+            element={
+              <ProtectedRoute>
+                <AdminMerch />
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
+      </div>
     </>
   );
 }
