bdedaa2d12fc0c89e287b3940219b61b7b4ae48a
lcroce28 <lcj10@alumnes.udl.cat>
2026-08-03 22:13:03 +0200

    changeessss

--- src/components/Home/StatsSecion.js
+++ src/components/Home/StatsSecion.js
@@ -18,25 +18,16 @@ export default function StatsSection() {
 
   return (
     <section className="stats-section">
-
       <p>EL DRAC EN XIFRES</p>
-
       <h2>Més de vint anys fent cremar Olesa</h2>
-
       <div className="stats-grid">
-
         {stats.map((stat) => (
           <div className="stat-card" key={stat.label}>
-
             <h3>{stat.number}</h3>
-
             <span>{stat.label}</span>
-
           </div>
         ))}
-
       </div>
-
     </section>
   );
 }
